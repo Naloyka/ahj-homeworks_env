e9ef60d8690c453947c8ed6b70c2e2e138f7779e
Anna Popova <panasenko-anna.8@mail.ru>
2022-09-07 20:59:02 +0300

    fix for eslint

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,5 +1,5 @@
 import calculate from '../app';
 
 test('test for function calculate', () => {
-	expect(calculate(3, 5)).toBe(8);
+  expect(calculate(3, 5)).toBe(8);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,4 @@ export default function calculate(a, b) {
   return a + b;
 }
 
-
 console.log('app.js is working!');
